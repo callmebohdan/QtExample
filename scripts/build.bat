4c76908040a032d493aa0e6a2ddace3952c03150
callmebohdan <bohdan.ponomarenko.work@gmail.com>
2024-09-25 20:42:40 +0300

    Refactor build script

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,79 +1,70 @@
 @echo off
+
 setlocal
 
-REM Save the current directory
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
 pushd %~dp0
-
-REM Navigate up one level from the scripts directory
 cd ..
-
-REM Store the current directory (project root) in PROJECT_DIR
 set PROJECT_DIR=%cd%
-
-REM Return to the original directory (scripts directory)
 popd
 
+set BIN_DIR=%PROJECT_DIR%\bin
 set BUILD_DIR=%PROJECT_DIR%\build
 set OUT_DIR=%PROJECT_DIR%\out
-set BIN_DIR=%PROJECT_DIR%\bin
-set QT_PATH=C:\Qt\6.7.2\mingw_64
-
-:: Clean previous build if exists
-if exist "%BUILD_DIR%" (
-    echo Cleaning previous build...
-    rd /s /q "%BUILD_DIR%"
-)
-
-:: Clean previous build if exists
-if exist "%OUT_DIR%" (
-    echo Cleaning previous build's output...
-    rd /s /q "%OUT_DIR%"
-)
+set QT_PATH=C:\Qt\6.7.2\msvc2019_64
+set SRC_DIR=%PROJECT_DIR%\src
 
 if exist "%BIN_DIR%" (
-    echo Cleaning previous build bin/...
+    echo Cleaning bin directory...
     rd /s /q "%BIN_DIR%"
 )
 
-:: Create build directory and navigate into it
+if exist "%BUILD_DIR%" (
+    echo Cleaning build directory...
+    rd /s /q "%BUILD_DIR%"
+)
+
+if exist "%OUT_DIR%" (
+    echo Cleaning out directory...
+    rd /s /q "%OUT_DIR%"
+)
+
 echo Creating build directory...
 mkdir "%BUILD_DIR%"
 cd "%BUILD_DIR%"
 
-:: Configure the project with CMake
-echo Running CMake configuration...
-cmake -G "MinGW Makefiles" -DCMAKE_PREFIX_PATH="%QT_PATH%" "%PROJECT_DIR%"
+if exist "%QT_PATH%\bin\uic.exe" (
+    echo Generating QtExample.h from QtExample.ui...
+    "%QT_PATH%\bin\uic.exe" -o "%SRC_DIR%\UI_QtExample.h" "%SRC_DIR%\QtExample.ui"
+)
 
-:: Build the project
-echo Building the project...
-mingw32-make
+echo Configuring the project with CMake...
+cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_PREFIX_PATH="%QT_PATH%" "%PROJECT_DIR%"
 
-:: Deploy Qt libraries if using Qt
+echo Building the project with MSBuild...
+msbuild "%BUILD_DIR%\QtExample.sln" /p:Configuration=Release
+
 if exist "%QT_PATH%\bin\windeployqt.exe" (
     echo Deploying Qt libraries...
-    "%QT_PATH%\bin\windeployqt.exe" "%BUILD_DIR%\QtExample.exe"
+    "%QT_PATH%\bin\windeployqt.exe" --release --compiler-runtime "%BUILD_DIR%\Release\QtExample.exe"
 )
 
-:: Copying the executable and necessary libraries into the bin directory
-if exist "%BUILD_DIR%\QtExample.exe" (
+if exist "%BUILD_DIR%\Release\QtExample.exe" (
     echo Copying the application into the bin/ directory...
-    mkdir "%BIN_DIR%\platforms"
-    copy "%BUILD_DIR%\QtExample.exe" "%BIN_DIR%"
-    copy "%BUILD_DIR%\Qt6Core.dll" "%BIN_DIR%"
-    copy "%BUILD_DIR%\Qt6Gui.dll" "%BIN_DIR%"
-    copy "%BUILD_DIR%\Qt6Widgets.dll" "%BIN_DIR%"
-    copy "%BUILD_DIR%\platforms" "%BIN_DIR%\platforms"
-) else (
-    echo Error: minipass.exe was not created.
-)
-
-:: Run the executable if it was created
-if exist "%BUILD_DIR%\QtExample.exe" (
-    echo Running the application...
-    "%BIN_DIR%\QtExample.exe"
+    mkdir "%BIN_DIR%"
+    xcopy /s /e /y "%BUILD_DIR%\Release\QtExample.exe" "%BIN_DIR%"
+    xcopy /s /e /y "%BUILD_DIR%\Release\*.dll" "%BIN_DIR%"
+    xcopy /s /e /y "%BUILD_DIR%\Release\generic" "%BIN_DIR%\generic"
+    xcopy /s /e /y "%BUILD_DIR%\Release\iconengines" "%BIN_DIR%\iconengines"
+    xcopy /s /e /y "%BUILD_DIR%\Release\imageformats" "%BIN_DIR%\imageformats"
+    xcopy /s /e /y "%BUILD_DIR%\Release\multimedia" "%BIN_DIR%\multimedia"
+    xcopy /s /e /y "%BUILD_DIR%\Release\networkinformation" "%BIN_DIR%\networkinformation"
+    xcopy /s /e /y "%BUILD_DIR%\Release\platforms" "%BIN_DIR%\platforms"
+    xcopy /s /e /y "%BUILD_DIR%\Release\styles" "%BIN_DIR%\styles"
+    xcopy /s /e /y "%BUILD_DIR%\Release\tls" "%BIN_DIR%\tls"
 ) else (
     echo Error: QtExample.exe was not created.
 )
 
 endlocal
-pause
