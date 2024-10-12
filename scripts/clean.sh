37b911255f922bae096c0b07bc2b4c95c8ebb00f
callmebohdan <bohdan.ponomarenko.work@gmail.com>
2024-10-12 22:51:18 +0300

    Add cleanup for ui_QtExample.h

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -6,6 +6,7 @@ PROJECT_DIR=$(pwd)
 
 BUILD_DIR="$PROJECT_DIR/build"
 BIN_DIR="$PROJECT_DIR/bin"
+SRC_DIR="$PROJECT_DIR/src"
 
 # Clean previous build if exists
 if [ -d "$BUILD_DIR" ]; then
@@ -17,4 +18,9 @@ fi
 if [ -d "$BIN_DIR" ]; then
     echo "Cleaning previous build bin/..."
     rm -rf "$BIN_DIR"
+fi
+
+if [ -d "SRC_DIR/ui_qtexample.h" ]; then
+    echo "Cleaning previous build ui_qtexample.h..."
+    rm -f "$BIN_DIR/ui_qtexample.h"
 fi
